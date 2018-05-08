9946b3c7611128c459bfeddb694b694a56e244bd
Jackson Tubbs-150002955 <150002955@vusdnoc.vusd.k12.ca.us>
2018-05-08 11:58:50 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var routes = require("./routes")
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(clientSessions({
+  secret: 'fjpasidfjpasifj'
+}));
+
+app.use('/', express.static('./'));
+app.use('/js', express.static('./public/js'));
+app.use('/views', express.static('./public/views'));
+app.use('/stylesheets', express.static('./public/stylesheets'));
+app.use('/images', express.static('./public/images'));
+
+app.set("view engine", "ejs");
+var port = process.env.PORT || 3000;
+app.listen(port,function(){
+
+});
