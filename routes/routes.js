1139dec948b366d0307061841b9b97319d19a9e3
Jarrett Koran-157002532 <157002532@vusdnoc.vusd.k12.ca.us>
2018-05-08 12:54:23 -0700

    path and passport

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,11 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 
+var passport = require("passport");
+var path = require("path");
+
+var User = require("./models/user");
+
 app.set("view engine", "ejs");
 
 router.get("/", function(req,res){
